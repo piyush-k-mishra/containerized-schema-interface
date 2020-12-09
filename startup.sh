3aa66982c3f69dd65cd8138cfba9557cd2093c12
piyush-k-mishra <piyush.mishra@colorado.edu>
2020-12-09 16:06:45 +0000

    Included rabbitmq

--- startup.sh
+++ startup.sh
@@ -1,3 +1,10 @@
+kubectl apply -f rabbitmq/rabbitmq-deployment.yaml
+kubectl apply -f rabbitmq/rabbitmq-service.yaml
+
+kubectl port-forward --address 0.0.0.0 service/rabbitmq 5672:5672 &
+kubectl port-forward --address 0.0.0.0 service/redis 6379:6379 &
+
 kubectl apply -f application/rest-deployment.yaml
+kubectl apply -f application/logs-deployment.yaml
 kubectl apply -f application/service.yaml
 kubectl apply -f application/ingress.yaml
